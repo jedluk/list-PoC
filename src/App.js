691ae10760049f425652353d4e4799f228a61f5c
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-12-07 13:10:38 +0100

    react-window PoC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef } from "react";
+import { FixedSizeList as List } from "react-window";
+import "./App.css";
+
+const list = Array.from(Array(3000).keys()).map(
+  (value) => "Some text " + value
+);
+
+const Row = ({ index, style }) => {
+  return <div style={style}> {list[index + 1]}</div>;
+};
 
 function App() {
+  const headerRef = useRef(null);
+  const handleScroll = useCallback(({ scrollOffset }) => {
+    const x = 100 - scrollOffset;
+    if (x > 30) {
+      headerRef.current.style.height = x + "px";
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <div ref={headerRef} className="header">
+          Subheader or sth
+        </div>
+        <List
+          height={500}
+          itemCount={list.length}
+          itemSize={35}
+          onScroll={handleScroll}
+          width={300}
         >
-          Learn React
-        </a>
-      </header>
+          {Row}
+        </List>
+      </div>
     </div>
   );
 }
